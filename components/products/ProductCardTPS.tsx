cf0dc453a2f5bfd624889b7b8b4e6b9824808cf2
nanoca031212 <lucasnatanael542@gmail.com>
2026-05-30 20:05:05 -0300

    bundle

--- components/products/ProductCardTPS.tsx
+++ components/products/ProductCardTPS.tsx
@@ -432,7 +432,7 @@ export default function ProductCardTPS({
 
           {/* Promotional Banner */}
           <div className="bg-white border border-black text-center font-bold text-xs py-1 px-2 mb-2">
-            Mix & match any 3 fragrances — £89.99 for all three
+            Summer deal buy 2 fragrances for $99.99 & get the 3rd FREE
           </div>
 
           {/* Badge - Canto superior direito */}
